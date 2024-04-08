be33d846562681146c9ce9e9a736aed2a71ab34b
hugodlrll <95039355+hugodlrll@users.noreply.github.com>
2024-04-08 09:43:27 +0000

    Partie 2 terminée

--- main2.cpp
+++ main2.cpp
@@ -1,10 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 /*
 ------------------ Namespace ------------------
 */
 using namespace std;
+
+/*
+------------------ Fonctions ------------------
+*/
+
+/*
+    Fonction qui permet l'affichage d'un vecteur en le passant par référence
+*/
+void affiche(vector<string> const& vs)
+{
+    for(auto i : vs) // Affichage du contenu du vecteur sur une ligne, séparé par des virgules
+    {
+        cout << i << " + ";
+    }
+    cout << endl;
+}
+
+/*
+    Fonction qui permet de concaténer un vecteur string dans une variable
+*/
+string concatene(vector<string> const& vs)
+{
+    string out;
+    for(auto i : vs)
+    {
+        out += i + " ";
+    }
+    return out;
+}
+
+/*
+    Fonction qui permet d'ajouter une virgule après chaque string
+*/
+void ajouteVirgule(vector<string> &vs)
+{
+    auto ajout = [](string& s) { s += ","; }; // prédicat
+    for_each(vs.begin(), vs.end(), ajout);
+}
 
 
 /*
@@ -12,21 +51,42 @@ using namespace std;
 */
 int main ()
 {
-    vector<int> v; // déclaration d'un vecteur d'entiers
-    v.resize(3); // redimensionnement du vecteur
-    v.assign(3,4); // affectation 3 valeurs qui sont 4
-    for(int i = 0; i < v.size(); i++) // affichage avec les indices
+    vector<string> vs; // Déclaration du vecteur de string
+    vs.push_back("Bonjour"); // Ajout des strings
+    vs.push_back("comment");
+    vs.push_back("allez");
+    vs.push_back("vous");
+    vs.push_back("?");
+    cout << "taille du vecteur : " << vs.size() << endl; // Affichage de la taille du vecteur
+    cout << "capacité du vecteur : " << vs.capacity() << endl; // Affichage de la capacité du vecteur
+    for(vector<string>::iterator it = vs.begin(); it != vs.end(); it++) // Affichage du contenu du vecteur sur une ligne
     {
-        cout << v[i] << endl;
-    }
-    for(auto j : v) // affichage avec la boucle de nombres
+        cout << *it << " ";
+    } 
+    cout << endl;
+    vs[1].swap(vs[3]); // Echange entre les valeurs des cases 1 et 3
+    vs.insert(vs.begin() + 1, "a tous"); // Ajout de la valeur "a tous" entre les cellules 1 et 3
+    vs.back() = "???"; // Remplace le dernier élément "?" par "???"
+    cout << "Dernier élément : " << *(vs.end() - 1) << endl; // Affichage du dernier élément
+    for(vector<string>::iterator it = vs.begin(); it != vs.end(); it++) // Affichage du contenu du vecteur sur une ligne, séparé par des virgules
     {
-        cout << j << endl;
+        cout << *it << ", ";
     }
-    for(vector<int>::iterator it = v.begin(); it != v.end(); it++) // affichage avec les itérateurs
+    cout << endl; 
+    sort(vs.begin(), vs.end()); // Trie des valeurs dans l'ordre alphabétique en fonction des priorités
+    for(vector<string>::iterator it = vs.begin(); it != vs.end(); it++) // Affichage du contenu du vecteur sur une ligne, séparé par des virgules
     {
-        cout << *it << endl;
+        cout << *it << ", ";
     }
-    return 0;
+    cout << endl;
+    cout << "Test de la fonction affiche : ";
+    affiche(vs);
+    cout << "Test de la fonction concatene : " << concatene(vs) << endl;
+    cout << "Test de la fonction ajouteVirgule : ";
+    ajouteVirgule(vs);
+    affiche(vs);
 }
+
+
+
 
